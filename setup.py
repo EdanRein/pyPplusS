59ca80242da76a6e595cbe64298423339ae3b1cf
EdanRein <edanrein2000@gmail.com>
2020-12-14 18:34:22 +0200

    Package-related file creation

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyppluss", # Replace with your own username
+    version="0.1.4",
+    author="Edan Rein",
+    author_email="edanrein2000@gmail.com",
+    description="Models for transiting exoplanets with rings",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EdanRein/pyPplusS",
+    license="MIT",
+    packages=['pyppluss'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    install_requires=[
+        "numpy",
+        "scipy"
+    ]
+)
